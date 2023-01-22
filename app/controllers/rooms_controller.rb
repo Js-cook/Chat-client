22abb57c77cdbf701400ea32f45514038837caf4
Jacob Cook <102179895+Js-cook@users.noreply.github.com>
2023-01-22 23:17:29 +0000

    ADD - Added basic CRUD functionality to app

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,14 +8,26 @@ class RoomsController < ApplicationController
   end
 
   def new
+    @room = Room.new
   end
 
   def create
+    @room = Room.new(room_params)
+    @room.save ? redirect_to(room_path(@room)) : render("new")
   end
 
   def edit
+    @room = Room.find(params[:id])
   end
 
   def update
+    @room = Room.find(params[:id])
+    @room.update(room_params) ? redirect_to(room_path(@room)) : render("edit")
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :description)
   end
 end
